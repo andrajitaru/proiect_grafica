78e043ee3d65a2008ca860135855ab77dddc6e4f
Andra Jitaru <andra.jitaru@gmail.com>
2016-06-12 20:03:06 +0300

    done

--- scripts.js
+++ scripts.js
@@ -8,10 +8,10 @@ function validateForm() {
     }
 }
 
-function colorNumberYellow() {
+function colorNumberRed() {
 	var elements = document.getElementsByClassName("co2value")
 	for (var i=0; i<elements.length; i++) {
-		elements[i].style.color="yellow"
+		elements[i].style.color="red"
 	}
 }   
 
@@ -21,3 +21,22 @@ function colorNumberBlack() {
 		elements[i].style.color="black"
 	}
 }      
+
+function addRow() {
+	var textNode=document.createTextNode("OECD members");
+	var newRegion=document.createElement("TD");
+	newRegion.appendChild(textNode);
+	
+	var numberNode=document.createTextNode("9.9");
+	var newValue=document.createElement("TD");
+	newValue.appendChild(numberNode);
+	
+	var newRow=document.createElement("TR");
+	newRow.appendChild(newRegion);
+	newRow.appendChild(newValue);
+	
+	var tableBody = document.getElementById("tbody");
+	tableBody.appendChild(newRow);
+	
+}
+     
